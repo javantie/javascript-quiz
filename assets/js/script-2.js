dc4f3b96729c4488e4b49806fbf03dd2a9b7a3d3
Akeem C <javantieclarke@gmail.com>
2021-06-19 16:13:18 -0400

    updated enhanced features

--- assets/js/script-2.js
+++ assets/js/script-2.js
@@ -1,39 +1,37 @@
-var scoreDisplayEl = document.getElementById("score-display")
-var deleteScoreEl = document.getElementById("delete-score")
-var backBtnEL= document.getElementById("back-btn")
-var scoreLink = document.getElementById("score-link")
+var scoreDisplayEl = document.getElementById("score-display");
+var deleteScoreEl = document.getElementById("delete-score");
+var backBtnEL = document.getElementById("back-btn");
+var scoreLink = document.getElementById("score-link");
 
-var namePlayer = localStorage.getItem("playerName")
-var score = localStorage.getItem("score")
+var removeScore = function () {
+  localStorage.clear();
+  console.log("This button works");
+};
 
+deleteScoreEl.addEventListener("click", removeScore);
 
 var getData = function () {
-    //var namePlayer = localStorage.getItem("playerName")
-     var palyerStat =JSON.parse(localStorage.getItem("highscore"))
-    //var score = localStorage.getItem("score")
-    if (namePlayer === null || score === null) {
-      return;
-    }
-    for (var i = 0; palyerStat.length; i++){
-      palyerStat[i]
-      var scoreEl= document.createElement("p");
-      scoreEl.textContent = palyerStat[i].playerName + ": " + palyerStat[i].score;
-      scoreDisplayEl.append(scoreEl);
-      console.log()
-    }
-    console.log("This button works")
-    console.log(namePlayer)
-    console.log(score)
-    console.log(palyerStat)
+  //var namePlayer = localStorage.getItem("playerName")
+  var palyerStat = JSON.parse(localStorage.getItem("highscore"));
+  //var score = localStorage.getItem("score")
+  if (palyerStat === null) {
+    return;
+  }
+  for (var i = 0; palyerStat.length; i++) {
+    palyerStat[i];
+    var scoreEl = document.createElement("p");
+    scoreEl.textContent = palyerStat[i].playerName + ": " + palyerStat[i].score;
+    scoreDisplayEl.append(scoreEl);
+  }
+  console.log("This button works");
+  console.log(namePlayer);
+  console.log(score);
+  console.log(palyerStat);
 };
 getData();
+
 var reSet = function () {
-    location.reload();
+  location.reload();
 };
-var removeScore = function () {
-    localStorage.clear();
-  };
 
-
-deleteScoreEl.addEventListener("click", removeScore)
-backBtnEL.addEventListener("click", reSet)
+backBtnEL.addEventListener("click", reSet);
